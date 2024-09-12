979449f591b01de6dd9e87f894eb7db686400bff
Dyrnax <marco.calfileo@gmail.com>
2024-09-12 14:37:54 -0300

    libro

--- Biblioteca/Clases/Libro.py
+++ Biblioteca/Clases/Libro.py
@@ -1,6 +1,6 @@
 class Libro:
-    def __init__(self,isbn,titulo,autor,n_copias):
-        self.isbn = isbn
-        self.titulo = titulo
-        self.autor = autor
-        self.ncopias = n_copias
+    def __init__(self,isbn,titulo,id_autor,n_copias):
+        self.__isbn = isbn
+        self.__titulo = titulo
+        self.__autor = id_autor
+        self.__ncopias = n_copias
