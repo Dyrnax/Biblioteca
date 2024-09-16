2b4192c9cd210fc32e371fb1e160b458936b7501
Marco Calfileo <marco.calfileo@gmail.com>
2024-09-16 11:30:36 -0300

    clases

--- Biblioteca/Clases/Detalle Libro.py
+++ Biblioteca/Clases/Detalle Libro.py
@@ -1,6 +1,6 @@
 class DetalleLibro:
     def __init__(self,isbn,categoria,n_paginas,id_editorial):
         self.__isbn = isbn
+        self.__editorial = id_editorial
         self.__categoria = categoria
         self.__npaginas = n_paginas
-        self.__editorial = id_editorial
--- Biblioteca/Clases/Usuarios.py
+++ Biblioteca/Clases/Usuarios.py
@@ -1,7 +1,7 @@
 class Usuario:
     def __init__(self,nombre,n_identificador,correo,telefono,habilitado):
         self.__nombre = nombre
-        self.__idusuario = n_identificador
+        self.__n_identificador = n_identificador
         self.__correo = correo
         self.__telefono = telefono
         self.__habilitado = habilitado
